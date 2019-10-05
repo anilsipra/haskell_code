94ac27a8fd55b8d6ba67b08cdd6dde17fe2a05b9
Rachit <rachit@uchicago.edu>
2019-10-05 17:06:36 -0500

    Print out my info

--- lab0/Hello.hs
+++ lab0/Hello.hs
@@ -20,3 +20,13 @@ why = "To develop skill in programming and learn haskell which is not a common a
 
 distance :: Int -> Int -> Int
 distance rate time = rate * time
+main :: IO ()
+main = do
+    putStrLn "Hello, world!"
+    putStrLn ""
+    putStrLn ("My name is " ++ name ++ ".")
+    putStrLn ("I am a " ++ show level ++ " student.")
+    putStrLn ("I'm going into " ++ major ++ ".")
+    putStrLn ("I'm in this class because " ++ why)
+    putStrLn ""
+    putStrLn ("If you travel for 15mph for 30 hours you will go " ++ show (distance 15 30) ++ " miles.")
