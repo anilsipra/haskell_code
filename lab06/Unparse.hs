b2d41edcc7bd308cf78983143042d0e3a51efe33
Rachit <rachit@uchicago.edu>
2019-11-27 18:26:26 -0600

    re

--- lab06/Unparse.hs
+++ lab06/Unparse.hs
@@ -12,12 +12,15 @@ import           Lab6
 unparse :: MathExp -> String
 unparse (LetE names assigns main) =
     "let " ++ commaList names ++ " = " ++
-    (commaList . map unparseMathExp $ assigns) ++ " in " ++
-    unparseMathExp main
+    (commaList . map unparse $ assigns) ++ " in " ++
+    unparse main
     where
         commaList [str] =  str
         commaList strs  = "(" ++ intercalate ", " strs ++ ")"
-unparse ( mathExp)           = unparseMathExp mathExp
+unparse (IF _ _ _ ) = ""
+unparse (Fun _ _ _ ) = ""
+unparse (mathExp)           = unparseMathExp mathExp
+
 
 unparseMathExp :: MathExp -> String
 unparseMathExp (Number n)     = show n
@@ -28,5 +31,10 @@ unparseMathExp (Minus  e1 e2) = "(" ++ unparseMathExp e1 ++ " - " ++ unparseMath
 unparseMathExp (Mult   e1 e2) = "(" ++ unparseMathExp e1 ++ " * " ++ unparseMathExp e2 ++ ")"
 unparseMathExp (Div    e1 e2) = "(" ++ unparseMathExp e1 ++ " / " ++ unparseMathExp e2 ++ ")"
 unparseMathExp (Pow    e1 e2) = "(" ++ unparseMathExp e1 ++  "^"  ++ unparseMathExp e2 ++ ")"
-
-
+unparseMathExp (Greater e1 e2) = "(" ++ unparseMathExp e1 ++  ">"  ++ unparseMathExp e2 ++ ")"
+unparseMathExp (Lesser e1 e2) = "(" ++ unparseMathExp e1 ++  "<"  ++ unparseMathExp e2 ++ ")"
+unparseMathExp (GorE e1 e2) = "(" ++ unparseMathExp e1 ++  ">="  ++ unparseMathExp e2 ++ ")"
+unparseMathExp (LorE e1 e2) = "(" ++ unparseMathExp e1 ++  "<="  ++ unparseMathExp e2 ++ ")"
+unparseMathExp (Equalto e1 e2) = "(" ++ unparseMathExp e1 ++  "=="  ++ unparseMathExp e2 ++ ")"
+unparseMathExp (NotE e1 e2) = "(" ++ unparseMathExp e1 ++  "/="  ++ unparseMathExp e2 ++ ")"
+unparseMathExp (Not e1 ) = "not" ++ unparseMathExp e1 
